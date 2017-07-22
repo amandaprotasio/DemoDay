7d40a3fc7a2858aa2e69a79f69c678ac9aade688
feliperrothschild <felipe.rothschild@hotmail.com>
2017-07-22 12:01:05 -0300

    Alterações no bloco de reviews no home - JS
    1) Na home.html, o final puxa os exemplos cadastrados no firebase ao invés de estarem escritos no arquivo. O código puxa aleatoriamente as reviews e no máximo 4 (se tiver mais que 4 cadastrados, 4 são escolhidos aleatoriamente). Problema: essa aleatorização está entrando em conflito com o número de corações do score, mostrando nota 5 para todos, inclusive aqueles com menos que 5 na nota.

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -1,5 +1,13 @@
 angular.module('seraqueehbom').controller("HomeController", HomeController);
 
-function HomeController(){
+function HomeController($firebaseArray, $scope, $filter){
+  var ref = firebase.database().ref('reviews');
+  $scope.reviews = $firebaseArray(ref);
+
+  $scope.random = function() {
+       return 0.5 - Math.random();
+   }
+   console.log($scope.random);
+
 
 }
